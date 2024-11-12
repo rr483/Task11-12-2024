daf224fe67fcdb4de735f15913802915991d909b
rr483 <rryy5879@gmail.com>
2024-11-12 11:03:20 +0500

    fc

---
+++ subhan.js
@@ -0,0 +1,11 @@
+let marks= prompt(('enter your marks'));
+console.log(marks);
+let Grade;
+if (marks >= 1100)console.log("Out of range")
+else if(marks >= 750 ){Grade="a";}
+else if
+(marks >= 700 ){console.log ("b");}
+else if(marks >= 650){Grade="c";}
+else if(marks >= 600){Grade="d";}
+else(console.log("fail"));
+console.log(Grade);
